ea25b7b689f9c7f884fe52e64c3748d543d44a97
m6parker <mparker0693@gmail.com>
2022-12-01 19:51:33 -0500

    finished testing, all pass!!!

--- test_functions.py
+++ test_functions.py
@@ -1,14 +1,11 @@
 """this module tests out the python functions in the program to make sure they are
 behaving as expected. """
 import pytest
-
 from testfixtures import TempDirectory
 
 import print_functions
 import util_funcs
 from print_functions import *
-from io import StringIO
-# import testfixtures
 
 
 def test_print_separation_line(capfd):
@@ -19,11 +16,22 @@ def test_print_separation_line(capfd):
     print_functions.print_separation_line('=', 2)
     out, err = capfd.readouterr()
     assert out == '\n\t==\n\n'
-    # assert print_separation_line('=', 2) == '\n\t==\n\n'
-    # assert print_separation_line('=', 0) == '\n\t\n\n'
-    # assert print_separation_line('=', 10) == '\n\t==========\n\n'
-    # assert print_separation_line('', 2) == '\n\t\n\n'
-    # assert print_separation_line('=', -1) is None
+
+    print_functions.print_separation_line('=', 0)
+    out, err = capfd.readouterr()
+    assert out == '\n\t\n\n'
+
+    print_functions.print_separation_line('=', 10)
+    out, err = capfd.readouterr()
+    assert out == '\n\t==========\n\n'
+
+    print_functions.print_separation_line('', 2)
+    out, err = capfd.readouterr()
+    assert out == '\n\t\n\n'
+
+    print_functions.print_separation_line('=', -1)
+    out, err = capfd.readouterr()
+    assert out == '\n\t\n\n'
 
 
 def test_extract_msg_file_content(capfd):
@@ -40,29 +48,29 @@ def test_extract_msg_file_content(capfd):
         test_message = b'test123'
         tempDir.write(temp_filename, test_message)
         file_path = tempDir.path + '\\' + temp_filename
-        util_funcs.extract_msg_file_content(file_path)
+        extract_msg_file_content(file_path)
     out, err = capfd.readouterr()
-    assert out == '-> test123\n'
+    assert out == ' -> test123\n'
 
     missing_file = 'missing_file.txt'
     with pytest.raises(FileNotFoundError) as error:
-        util_funcs.extract_msg_file_content(missing_file)
+        extract_msg_file_content(missing_file)
     assert error.type is FileNotFoundError
 
 
 def test_lock_depth_positive_check(capfd):
     """this function tests that the function that checks in the lock depth argument is valid"""
-    # depth = 0
-    # arg_val = '0'
-    # assert util_funcs.lock_depth_positive_check(depth, arg_val) is None
-    # out, err = capfd.readouterr()
-    # assert out == f'\n\tInvalid lock depth: \'{arg_val}\'. Must be an integer greater than 0 (zero).\n'
+    depth = 0
+    arg_val = '0'
+    assert util_funcs.lock_depth_positive_check(depth, arg_val) is None
+    out, err = capfd.readouterr()
+    assert out == f'\n\tInvalid lock depth: \'{arg_val}\'. Must be an integer greater than 0 (zero).\n\n'
 
-    # depth = -1
-    # arg_val = '-1'
-    # assert util_funcs.lock_depth_positive_check(depth, arg_val) is None
-    # out, err = capfd.readouterr()
-    # assert out == f'\n\tInvalid lock depth: \'{arg_val}\'. Must be an integer greater than 0 (zero).\n'
+    depth = -10
+    arg_val = '-10'
+    assert util_funcs.lock_depth_positive_check(depth, arg_val) is None
+    out, err = capfd.readouterr()
+    assert out == f'\n\tInvalid lock depth: \'{arg_val}\'. Must be an integer greater than 0 (zero).\n\n'
 
     depth = 20
     arg_val = '20'
